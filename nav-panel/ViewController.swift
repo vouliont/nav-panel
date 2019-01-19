6601da41ba5c40f212d1a253580f89aeec8a115d
vlad <vladporaiko@gmail.com>
2019-01-19 16:12:32 +0200

    Initial Commit

---
+++ nav-panel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  nav-panel
+//
+//  Created by Владислав on 1/19/19.
+//  Copyright © 2019 vladporaiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
